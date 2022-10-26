ee52c6644ce22129c316235547afb71c68277785
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2022-10-26 19:12:49 +0500

    remove: async header() function

--- next.config.js
+++ next.config.js
@@ -2,20 +2,7 @@ const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
-  async headers() {
-    return [
-      {
-        source: "/fonts/Dystopian Bold.woff2",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+  i18n
 };
 
 module.exports = nextConfig;
