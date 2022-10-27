39e7425c5651a3bd37ca6de3c7b09836335296fb
mkomil1 <komilmaxmudov9@gmail.com>
2022-10-27 16:09:16 +0500

    works add

--- components/works/index.jsx
+++ components/works/index.jsx
@@ -4,7 +4,7 @@ const Works = () => {
 
   return (
     <section className="">
-     <div className="flex flex-wrap justify-between items-center">
+      <div className="flex flex-wrap justify-between  pb-10 md:pb-14 lg:pb-16 xl:pb-20 items-center">
         <div>
           <h1 className="md:text-[4rem] text-[2rem] font-bold leading-none">
             Biz<span className="text-[#006B98]"> Nimalar</span>{' '}
@@ -18,6 +18,69 @@ const Works = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing  <br className="md:block hidden" /> Autem dolorem commodi iusto est rerum maxime quos
           </p>
         </div>
+      </div>
+
+      <div className='lg:flex justify-between border-t-[1px]  py-[30px] items-center  flex-wrap'>
+
+        <div className='flex items-center justify-between'>
+
+          <div className='rounded-full sm:w-[100px] sm:h-[100px] w-[70px] h-[70px] bg-[#3b3939] dark:bg-[#E1E1E1] p-[15px] block'>
+            <img className='w-full h-full dark:brightness-0' height src="/svg/star-white.svg" alt="" />
+          </div>
+
+          <h3 className='sm:text-[35px] text-[28px] xl:ml-[100px] lg:ml-[50px]  text-[#202020] dark:text-[white]'>UI/UX Desing</h3>
+
+        </div>
+
+        <div className='flex xl:mt-0 mt-[30px] justify-between lg:w-[43%] xl:ml-auto'>
+
+          <p className=''> Lorem ipsum dolor sit amet asim kahon  <br className='sm:block' hidden /> Corrupti omnis magnam, at explicabo </p>
+          <img className='w-[40px] h-[40px] sm:block hidden brightness-0 dark:brightness-100' height src="/svg/star-white.svg" alt="" />
+
+        </div>
+
+      </div>
+
+      <div className='lg:flex justify-between border-t-[1px]  py-[30px] items-center  flex-wrap'>
+
+        <div className='flex items-center justify-between'>
+
+          <div className='rounded-full sm:w-[100px] sm:h-[100px] w-[70px] h-[70px] bg-[#3b3939] dark:bg-[#E1E1E1] p-[15px] block'>
+            <img className='w-full h-full dark:brightness-0' height src="/svg/star-white.svg" alt="" />
+          </div>
+
+          <h3 className='sm:text-[35px] text-[28px] xl:ml-[100px] lg:ml-[50px]  text-[#202020] dark:text-[white]'>Logo Desing</h3>
+
+        </div>
+
+        <div className='flex xl:mt-0 mt-[30px] justify-between lg:w-[43%] xl:ml-auto'>
+
+          <p className=''> Lorem ipsum dolor sit amet asim kahon  <br className='sm:block' hidden /> Corrupti omnis magnam, at explicabo </p>
+          <img className='w-[40px] h-[40px] sm:block hidden brightness-0 dark:brightness-100' height src="/svg/star-white.svg" alt="" />
+
+        </div>
+
+      </div>
+
+      <div className='lg:flex justify-between border-t-[1px]  py-[30px] items-center  flex-wrap'>
+
+        <div className='flex items-center justify-between'>
+
+          <div className='rounded-full sm:w-[100px] sm:h-[100px] w-[70px] h-[70px] bg-[#3b3939] dark:bg-[#E1E1E1] p-[15px] block'>
+            <img className='w-full h-full dark:brightness-0' height src="/svg/star-white.svg" alt="" />
+          </div>
+
+          <h3 className='sm:text-[35px] text-[28px] xl:ml-[100px] lg:ml-[50px]  text-[#202020] dark:text-[white]'>Devolopment</h3>
+
+        </div>
+
+        <div className='flex xl:mt-0 mt-[30px] justify-between lg:w-[43%] xl:ml-auto'>
+
+          <p className=''> Lorem ipsum dolor sit amet asim kahon  <br className='sm:block' hidden /> Corrupti omnis magnam, at explicabo </p>
+          <img className='w-[40px] h-[40px] sm:block hidden brightness-0 dark:brightness-100' height src="/svg/star-white.svg" alt="" />
+
+        </div>
+
       </div>
 
     </section>
