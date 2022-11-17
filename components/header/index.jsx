75c9f746fe102439cf4f7967629401dcd9874672
Muzaffar <muzaffarsafarov880@gmail.com>
2022-11-17 12:30:16 +0500

    fix: performence

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -65,7 +65,7 @@ const Header = () => {
                   </div>
                 </div>
               </div>
-              <div className="hidden sm:block ml-2 md:mt-12 w-10 h-10 xl:w-20 xl:w-20 duration-500 hover:rotate-180">
+              <div className="hidden sm:block ml-2 md:mt-12 w-10 h-10 xl:h-20 xl:w-20 duration-500 hover:rotate-180">
                 <Image
                   src={star}
                   alt="dotSoft web programming group spinner"
@@ -88,7 +88,7 @@ const Header = () => {
             </div>
             <div className="hidden md:block max-w-2xl -space-y-1 text-lg font-semibold text-gray-700 dark:text-white">
               <p className="ml-5 sm:ml-10 md:ml-16 text-xs">
-              {t('home:heading_paragraph')}
+                {t('home:heading_paragraph')}
               </p>
             </div>
           </div>
